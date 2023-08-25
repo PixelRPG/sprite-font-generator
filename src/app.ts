cab1d02bd352e21dccd96a9707d88051f2c4a42d
Pascal Garber <pascal@artandcode.studio>
2023-08-25 12:13:39 +0200

    Get unicode characters by unicode range name

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,13 @@
 import { Engine, Text, Font, FontUnit, FontStyle, TextAlign, BaseAlign, Direction, ScreenElement, DisplayMode, Loader, Color, KeyEvent, Keys, FontOptions, GraphicOptions, RasterOptions } from 'excalibur';
 import { waitForFontLoad } from './utils';
-import { addHTMLTextFonts } from './html'
+import { addHTMLTextFonts } from './html';
+import { getUnicodeStringByNames } from './unicode';
 
 const loader = new Loader([]);
 
-const unicodeText =
-`ABCDEFGHIJKLMNOPQRSTUVWXYZ
-abcdefghijklmnopqrstuvwxyz
-0123456789 !"#$%&'()*+,-./
-:;<=>?@[\\]^_\`{|}~¡¢£¤¥¦§¨ª
-«¬¯°±²³´µ¶¸¹º»¿ÀÁÂÃÄÅÆÇÈÉÊ
-ËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãä
-åæçèéêëìíîïðñòóôõö÷øùúûüýþ
-ÿĀāĂăĆćĈĉĊċČčĐđĒēĔĕĖėĚěĜĝĞ
-ğĠġĨĩĪīĬĭŃńŇňŌōŎŏŔŕŨũŪūŬŭŴ
-ŵŶŷŸŹźŻżŽžƒǍǎǏǐǑǒǓǔǕǖǗǘǙǚǛ
-ǜǝǞǟǠǡǢǣǸǹǼǽȞȟȲȳ‑‒–‘’‚‛“”„
-‟․‥…‧‾⁋⁌⁍⁰ⁱ⁴⁵⁶⁷⁸⁹⁺⁻⁼⁽⁾ⁿ₀₁₂
-₃₄₅₆₇₈₉₊₋₌₍₎ₐₑₒₓₔₕₖₗₘₙₚₛₜ█`
+console.log("getUnicodeCharacterListByName", );
+
+const unicodeText = getUnicodeStringByNames(['Basic Latin', 'Latin-1 Supplement'], 10);
 
 const engine = new Engine({
   antialiasing: false,
@@ -25,15 +15,15 @@ const engine = new Engine({
   suppressPlayButton: true,
   backgroundColor: Color.White,
   displayMode: DisplayMode.Fixed,
-  width: 300,
-  height: 300,
+  width: 600,
+  height: 800,
   canvasElementId: 'canvas',
 });
 
 const baseFontOptions: FontOptions & GraphicOptions & RasterOptions = {
   unit: FontUnit.Px,
   quality: 1,
-  padding: 1, // 1px padding for rounding errors?
+  padding: 0, // for rounding errors?
   smoothing: false,
   style: FontStyle.Normal,
   bold: false,
@@ -43,91 +33,97 @@ const baseFontOptions: FontOptions & GraphicOptions & RasterOptions = {
   color: Color.Black,
 }
 
-const texts = [
+const fonts: FontOptions[] = [
   // Ark Pixel
-  new Text({
-    text: unicodeText,
-    font: new Font({
-      ...baseFontOptions,
-      size: 10,
-      family: 'ark-pixel-10px-monospaced-latin',
-    }),
-    maxWidth: engine.drawWidth,
-  }),
-  new Text({
-    text: unicodeText,
-    font: new Font({
-      ...baseFontOptions,
-      size: 12,
-      family: 'ark-pixel-12px-monospaced-latin',
-    }),
-    maxWidth: engine.drawWidth,
-  }),
-  new Text({
-    text: unicodeText,
-    font: new Font({
-      ...baseFontOptions,
-      size: 16,
-      family: 'ark-pixel-16px-monospaced-latin',
-    }),
-    maxWidth: engine.drawWidth,
-  }),
-
+  {
+    size: 10,
+    family: 'ark-pixel-10px-monospaced-latin',
+  },
+  {
+    size: 12,
+    family: 'ark-pixel-12px-monospaced-latin',
+  },
+  {
+    size: 16,
+    family: 'ark-pixel-16px-monospaced-latin',
+  },
   // Fusion Pixel
-  new Text({
-    text: unicodeText,
-    font: new Font({
-      ...baseFontOptions,
-      size: 8,
-      family: 'fusion-pixel-8px-monospaced-latin',
-    }),
-    maxWidth: engine.drawWidth,
-  }),
-  new Text({
-    text: unicodeText,
-    font: new Font({
-      ...baseFontOptions,
-      size: 10,
-      family: 'fusion-pixel-10px-monospaced-latin',
-    }),
-    maxWidth: engine.drawWidth,
-  }),
-  new Text({
-    text: unicodeText,
-    font: new Font({
-      ...baseFontOptions,
-      size: 12,
-      family: 'fusion-pixel-12px-monospaced-latin',
-    }),
-    maxWidth: engine.drawWidth,
-  }),
+
+  // 8px to small?
+  // {
+  //   size: 8,
+  //   family: 'fusion-pixel-8px-monospaced-latin',
+  // },
+  {
+    size: 10,
+    family: 'fusion-pixel-10px-monospaced-latin',
+  },
+  {
+    size: 12,
+    family: 'fusion-pixel-12px-monospaced-latin',
+  },
 ];
 
+const texts: Text[] = [];
+
+for (const fontData of fonts) {
+  texts.push(new Text({
+    text: unicodeText,
+    font: new Font({
+      ...baseFontOptions,
+      ...fontData,
+    }),
+    // maxWidth: engine.drawWidth,
+  }));
+}
 
 const actor = new ScreenElement({
-  width: engine.drawWidth,
-  height: engine.drawHeight,
+  // width: engine.drawWidth,
+  // height: engine.drawHeight,
 });
 
 for (const text of texts) {
   const size = (text.font as Font).size
   const family = (text.font as Font).family
-  await waitForFontLoad(`${size}px ${family}`); // e.g. '10px ark-pixel-10px-monospaced-latin'
+  await waitForFontLoad(`${size}px '${family}'`); // e.g. '10px ark-pixel-10px-monospaced-latin'
   actor.graphics.add(family, text);
 }
 
 let activeFontIndex = -1;
+
+const switchFont = (index: number) => {
+  if (index < 0 || index >= texts.length) {
+    return;
+  }
+  const text = texts[index];
+  const family = (text.font as Font).family;
+  const size = (text.font as Font).size;
+  console.info(`Switching to font ${family} ${size}px`);
+  actor.graphics.use(family);
+
+  // Set canvas size to match text size
+  const width = actor.graphics.getGraphic(family)?.width ?? 0;
+  const height = actor.graphics.getGraphic(family)?.height ?? 0;
+
+  console.debug('width', width);
+  console.debug('height', height);
+
+  engine.screen.viewport.width = width;
+  engine.screen.viewport.height = height;
+
+  engine.screen.resolution.width = width;
+  engine.screen.resolution.height = height;
+
+  engine.screen.applyResolutionAndViewport()
+
+}
 
 const nextFont = () => {
   ++activeFontIndex;
   if (activeFontIndex >= texts.length) {
     activeFontIndex = 0;
   }
-  const text = texts[activeFontIndex];
-  const family = (text.font as Font).family;
-  const size = (text.font as Font).size;
-  console.info(`Switching to font ${family} ${size}px`);
-  actor.graphics.use(family);
+  switchFont(activeFontIndex);
 }
 
 const prevFont = () => {
@@ -135,11 +131,7 @@ const prevFont = () => {
   if (activeFontIndex < 0) {
     activeFontIndex = texts.length - 1;
   }
-  const text = texts[activeFontIndex];
-  const family = (text.font as Font).family;
-  const size = (text.font as Font).size;
-  console.info(`Switching to font ${family} ${size}px`);
-  actor.graphics.use(family);
+  switchFont(activeFontIndex);
 }
 
 nextFont();
